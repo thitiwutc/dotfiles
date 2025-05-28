02a1b9c03c2565c30f3aae5143c7a40dc78140fd
Thitiwut Chutipongwanit <56450795+thitiwutc@users.noreply.github.com>
2025-05-28 22:24:48 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 
-dotfiles=(".vimrc" ".tmux.conf")
+dotfiles=(".vimrc" ".tmux.conf" ".gitconfig" ".aliases")
+
+if [[ $# -gt 0 ]]; then
+    dotfiles=()
+    for file in "$@"
+    do
+        dotfiles+=("$file")
+    done
+    echo "Deleting dotfiles: ${dotfiles[*]}"
+else
+    echo "Deleting default dotfiles: ${dotfiles[*]}"
+fi
+
+deleted=()
+failed=()
 
 for file in "${dotfiles[@]}"
 do
-    rm "$HOME/$file"
+    # Check if file deleted successfully.
+    if rm "$HOME/$file" 2> /dev/null; then
+        deleted+=("$file")
+    else
+        failed+=("$file")
+    fi
 done
+
+if [[ ${#deleted[@]} -gt 0 ]]; then
+    echo -e "Deleted: \033[0;32m${deleted[*]}\033[0m"
+fi
+
+if [[ ${#failed[@]} -gt 0 ]]; then
+    echo -e "Not exist: \033[0;33m${failed[*]}\033[0m"
+fi
 
