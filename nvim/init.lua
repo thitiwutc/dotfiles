fa5b161553d95964426b64e07ff6973e3863adcb
Thitiwut Chutipongwanit <56450795+thitiwutc@users.noreply.github.com>
2025-06-03 00:07:39 +0700

    Update scrollof to 10

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,7 +67,7 @@ vim.o.inccommand = "split"
 vim.o.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 7
+vim.o.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
