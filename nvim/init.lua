bf3120b97caf40fdac217a0270754016e827ebcd
Thitiwut Chutipongwanit <56450795+thitiwutc@users.noreply.github.com>
2025-06-03 00:07:19 +0700

    Disable cursorline

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,7 +64,7 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.o.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.o.cursorline = true
+vim.o.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 7
