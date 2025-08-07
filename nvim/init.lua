c981318ecf9c484081366b944784ec342d4783ed
Thitiwut Chutipongwanit <56450795+thitiwutc@users.noreply.github.com>
2025-08-07 12:42:49 +0700

    nvim: update timeoutlen

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,7 @@ vim.o.signcolumn = "yes"
 vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
+vim.o.timeoutlen = 500
 
 -- Configure how new splits should be opened
 vim.o.splitright = true
