fa54cf59f150fea5725c43eaffe031676125778c
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-08-12 00:16:36 +0700

    nvim: set html and go formatter

--- nvim/init.lua
+++ nvim/init.lua
@@ -630,6 +630,8 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				html = { "html_beautify" },
+				go = { "goimports" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
