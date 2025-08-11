551e12e10bb4e62b8a2131a434acc11aca0225fb
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-08-12 00:16:11 +0700

    nvim: enable html and ts_ls lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -534,6 +534,8 @@ require("lazy").setup({
 				clangd = {},
 				gopls = {},
 				pyright = {},
+				html = {},
+				ts_ls = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
