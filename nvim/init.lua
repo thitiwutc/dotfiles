8e55d867d7dbc672752132fec25a5c29d3a869b0
Thitiwut Chutipongwanit <thitiwut.c@extend-os.com>
2025-10-01 17:58:41 +0700

    update code action keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -396,7 +396,7 @@ require("lazy").setup({
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
-					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+					map("ga", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
