f6306643d33845e5d9fd3a01565341232a5e70ea
Thitiwut Chutipongwanit <thitiwut.c@extend-os.com>
2025-10-12 22:28:19 +0700

    nvim: run jdtls config only on java files

--- nvim/init.lua
+++ nvim/init.lua
@@ -612,12 +612,13 @@ require("lazy").setup({
 						-- certain features of an LSP (for example, turning off formatting for ts_ls)
 						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 						require("lspconfig")[server_name].setup(server)
+
+						if server_name == "jdtls" then
+							require("lsp.jdtls")
+						end
 					end,
 				},
 			})
-
-			-- Load jdtls config
-			require("lsp.jdtls")
 		end,
 	},
 
