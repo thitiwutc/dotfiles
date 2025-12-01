0288c42c1d132f524aaadf3553c40871a066660c
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-12-01 14:40:38 +0700

    nvim: enable vim.o.list

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,7 +69,7 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
-vim.o.list = false
+vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
