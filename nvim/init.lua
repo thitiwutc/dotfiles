e29e46d8d2901c20b233371cd42f1e65a47a3b0b
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-12-01 14:44:01 +0700

    nvim: add xml lang server

--- nvim/init.lua
+++ nvim/init.lua
@@ -570,6 +570,7 @@ require("lazy").setup({
 				graphql = {
 					filetypes = { "graphql", "graphqls", "gql" },
 				},
+				lemminx = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
