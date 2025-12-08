9465293bccd0c609924705f3b7ee4f82cda5d025
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-12-08 22:18:15 +0700

    nvim: add yamlls and clangd to lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -567,6 +567,8 @@ require("lazy").setup({
 					filetypes = { "graphql", "graphqls", "gql" },
 				},
 				lemminx = {},
+				yamlls = {},
+				clangd = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
