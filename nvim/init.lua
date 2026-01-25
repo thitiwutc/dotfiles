ff1e8bd0026fe3910174388678b08daf923b1a3d
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-01-25 17:44:37 +0700

    nvim: set termguicolors

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,8 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.o.termguicolors = true
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
