d6a43024452d066af284c3a74e21063b8ebd82cc
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-03-11 14:58:50 +0700

    nvim: update theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -906,7 +906,7 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-moon")
+			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 
