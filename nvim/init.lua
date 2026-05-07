9bf51e9d8d1c13532c0666423aecd6e8f48397cd
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-05-07 15:16:46 +0700

    auto format c and cpp files

--- nvim/init.lua
+++ nvim/init.lua
@@ -677,7 +677,7 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {}
 				if disable_filetypes[ft] then
 					return nil
 				end
