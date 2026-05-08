b0557bf85b143b531bcc79284d3ce30b02059a67
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-05-08 17:10:18 +0700

    remove scrolloff

--- nvim/init.lua
+++ nvim/init.lua
@@ -82,9 +82,6 @@ vim.o.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
