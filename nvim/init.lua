8368249fea56d3c0041f423982e27aa5f5a51966
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-05-09 09:11:34 +0700

    add tree-sitter-manager.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -173,6 +173,22 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
+	{
+		"romus204/tree-sitter-manager.nvim",
+		dependencies = {}, -- tree-sitter CLI must be installed system-wide
+		config = function()
+			require("tree-sitter-manager").setup({
+				-- Default Options
+				-- ensure_installed = {}, -- list of parsers to install at the start of a neovim session
+				-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+				-- auto_install = false, -- if enabled, install missing parsers when editing a new file
+				-- highlight = true, -- treesitter highlighting is enabled by default
+				-- languages = {}, -- override or add new parser sources
+				-- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+				-- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+			})
+		end,
+	},
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
