1a8a9af302848bb1aa7fca3aacf7eb0e9d77d72a
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-05-15 10:42:27 +0700

    remove iferr package

--- nvim/init.lua
+++ nvim/init.lua
@@ -576,10 +576,7 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- Golang
 				gopls = {},
-				iferr = {},
-
 				pyright = {},
 				html = {},
 				ts_ls = {},
