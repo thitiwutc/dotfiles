19f61303c0734715031a9993ebd2bcfe4d7752f0
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-05-17 15:50:05 +0700

    use prettier for typescriptreact filetype

--- nvim/init.lua
+++ nvim/init.lua
@@ -729,6 +729,7 @@ require("lazy").setup({
 				graphqls = { "prettier_graphql" },
 				sql = { "sql_formatter" },
 				markdown = { "markdownlint-cli2" },
+				typescriptreact = { "prettier" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
